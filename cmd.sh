fe18e9c3d01566008d4e6e631b7120231037665c
ayoul3 <ayoul3.zos@gmail.com>
2019-09-26 22:47:38 +0200

    changes made

--- cmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in 1 2 3 4 5
-do
-   echo "Welcome $i times">>/tmp/output
-done
--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-scriptInput = open("./utils/cloud.py", "r").read()
-out = subprocess.Popen(
-    ["python2", "-c", scriptInput], stdout=subprocess.PIPE
-).stdout.read()
-print(out)
